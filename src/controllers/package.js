5639a12763e678ceeeaf124e9ff5d4f0bf196bd9
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2019-10-07 20:31:05 -0400

    desctrutured package file

--- src/controllers/package.js
+++ src/controllers/package.js
@@ -7,15 +7,9 @@ import schema from '../schema/package';
 
 class PackageData {
     static async addPackage(req, res) {
-        const deliveryDate = req.body.deliveryDate;
-        const deliveryTimestamp = moment(deliveryDate, 'YYYY-MM-DD').unix()
-        const packageData = {
-            deliveryDate: deliveryTimestamp,
-            deliveryTime: req.body.deliveryTime,
-            additionalInfo: req.body.additionalInfo,
-            name: req.body.name,
-            companyName: req.body.companyName,
-        };
+        const packageData = { ...req.body };
+        const deliveryTimestamp = moment(packageData.deliveryDate, 'YYYY-MM-DD').unix()
+         packageData.deliveryDate = deliveryTimestamp
         try {
             const result = await schema.validateAsync(packageData);
             if (!result.error) {
