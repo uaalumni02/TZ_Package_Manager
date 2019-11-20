27e772e25e46f8e3155635fb5983dd52388a1cca
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2019-11-19 20:51:11 -0500

    add email confirmation

--- src/controllers/package.js
+++ src/controllers/package.js
@@ -1,71 +1,102 @@
-import Db from '../db/db';
-import Package from '../models/package';
+import Db from "../db/db";
+import Package from "../models/package";
 
-import * as Response from '../helpers/response/response';
-import validator from '../validator/package';
+import * as Response from "../helpers/response/response";
+import validator from "../validator/package";
 
-import moment from 'moment';
+import moment from "moment";
+import gmail from "node-gmailer";
 
 class PackageData {
-    static async addPackage(req, res) {
-        const packageData = { ...req.body };
-        const deliveryTimestamp = moment(packageData.deliveryDate, 'YYYY-MM-DD hh:mmA').unix()
-        packageData.deliveryDate = deliveryTimestamp
-        try {
-            const result = await validator.validateAsync(packageData);
-            if (!result.error) {
-                const packageInfo = await Db.addPackage(Package, packageData)
-                return Response.responseOkCreated(res, packageInfo)
-            }
-        } catch (error) {
-            return Response.responseServerError(res)
-        }
+  static async addPackage(req, res) {
+    const packageData = { ...req.body };
+    const deliveryTimestamp = moment(
+      packageData.deliveryDate,
+      "YYYY-MM-DD hh:mmA"
+    ).unix();
+    packageData.deliveryDate = deliveryTimestamp;
+    try {
+      const result = await validator.validateAsync(packageData);
+      if (!result.error) {
+        const packageInfo = await Db.addPackage(Package, packageData);
+        return Response.responseOkCreated(res, packageInfo);
+      }
+    } catch (error) {
+      return Response.responseServerError(res);
     }
-    static async getAllPackages(req, res) {
-        try {
-            const allPackages = await Db.getAllPackages(Package)
-            return Response.responseOk(res, allPackages)
-        } catch (error) {
-            return Response.responseNotFound(res)
-        }
+  }
+  static async getAllPackages(req, res) {
+    try {
+      const allPackages = await Db.getAllPackages(Package);
+      return Response.responseOk(res, allPackages);
+    } catch (error) {
+      return Response.responseNotFound(res);
     }
-    static async getPackageByResident(req, res) {
-        const { resident } = req.params;
-        try {
-            const packageByResident = await Db.getPackageByResident(Package, resident)
-            return Response.responseOk(res, packageByResident)
-        } catch (error) {
-            return Response.responseNotFound(res)
-        }
+  }
+  static async getPackageByResident(req, res) {
+    const { resident } = req.params;
+    try {
+      const packageByResident = await Db.getPackageByResident(
+        Package,
+        resident
+      );
+      return Response.responseOk(res, packageByResident);
+    } catch (error) {
+      return Response.responseNotFound(res);
     }
-    static async deletePackage(req, res) {
-        const { resident } = req.params;
-        try {
-            const packageToDelete = await Db.removePackage(Package, resident)
-            return Response.responseOk(res, packageToDelete)
-        } catch (error) {
-            return Response.responseServerError(res)
-        }
+  }
+  static async deletePackage(req, res) {
+    const { resident } = req.params;
+    try {
+      const packageToDelete = await Db.removePackage(Package, resident);
+      return Response.responseOk(res, packageToDelete);
+    } catch (error) {
+      return Response.responseServerError(res);
     }
-    static async getPackageByDate(req, res) {
-        const { deliveryDate } = req.params;
-        try {
-            const packageByDate = await Db.getPackageByDate(Package, deliveryDate)
-            return Response.responseOk(res, packageByDate)
-        } catch (error) {
-            return Response.responseNotFound(res)
-        }
+  }
+  static async getPackageByDate(req, res) {
+    const { deliveryDate } = req.params;
+    try {
+      const packageByDate = await Db.getPackageByDate(Package, deliveryDate);
+      return Response.responseOk(res, packageByDate);
+    } catch (error) {
+      return Response.responseNotFound(res);
     }
-    static async getPackageById(req, res) {
-        const { id } = req.params;
-        try {
-            const packageById = await Db.getPackageById(Package, id)
-            return Response.responseOk(res, packageById)
-        } catch (error) {
-            return Response.responseNotFound(res)
-        }
+  }
+  static async getPackageById(req, res) {
+    const { id } = req.params;
+    try {
+      const packageById = await Db.getPackageById(Package, id);
+      const dateString = moment
+        .unix(packageById.deliveryDate)
+        .format("MM/DD/YYYY hh:mmA");
+      const customerMessage =
+        "Hi " +
+        packageById.name.name +
+        " " +
+        " you have a package from " +
+        packageById.companyName.companyName +
+        " " +
+        "deliveried on" +
+        " " +
+        dateString + ' ' + 'Addtional information: ' + '' + packageById.additionalInfo
+      const recipient = process.env.GMAIL_ADDRESS;
+      const messageData = {
+        subject: "Package Delivery Confirmation",
+        text: customerMessage
+      };
+      const sendHandler = () => {
+        gmail
+          .send(recipient, messageData)
+          .then(response => {})
+          .catch(error => {});
+      };
+      sendHandler();
+      return Response.responseOk(res, packageById);
+    } catch (error) {
+      return Response.responseNotFound(res);
     }
+  }
 }
-
 
 export default PackageData;
